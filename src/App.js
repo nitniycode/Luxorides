d38077617fcb01cc33ef569b0ab2b20a943281b0
nitin yadav <ny941078@gmail.com>
2023-02-08 13:10:19 +0530

    Designed Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from './pages/Explore'
+import Home from './pages/Home'
+import Location from './pages/Locations'
+import More from './pages/More'
+import Support from './pages/Support'
+import Header from "./components/Header";
 
-export default function App() {
+
+function App() {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/location" element={<Location />} />
+          <Route path="/more" element={<More />} />
+          <Route path="/support" element={<Support />} />
+        </Routes>
+      </Router>
+    </>
+
+
   )
 }
-
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='bg-white border-b-2 shadow-sm sticky top-0 z-50'>
+        <header className='flex justify-between items-center px-5 max-w-6xl mx-auto'>
+            <div>
+                <img src="https://luxorides.com/assets/images/luxorides-square-logo-only-solid-whitepng-400ppi-13-500x500.webp" 
+                     alt="Luxorides"
+                     className='h-5'
+                />
+            </div>
+            <div>
+                <ul className='flex space-x-6'>
+                    <li className='py-3 text-slate-500 text-sm font-semibold'>Home</li>
+                    <li className='py-3 text-slate-500 text-sm font-semibold'>Explore</li>
+                    <li className='py-3 text-slate-500 text-sm font-semibold'>Locations</li>
+                    <li className='py-3 text-slate-500 text-sm font-semibold'>Support</li>
+                    <li className='py-3 text-slate-500 text-sm font-semibold'>More</li>
+                </ul>
+            </div>
+        </header>
+
+    </div>
   )
 }
